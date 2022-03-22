df8757042af92c60d3a974270db7c2bb1b1feded
jamesmoraless <71559183+jamesmoraless@users.noreply.github.com>
2022-03-21 20:24:54 -0400

    Minion spots moving

--- Restorations of Eldya/Assets/__Scripts/FireMinionMove.cs
+++ Restorations of Eldya/Assets/__Scripts/FireMinionMove.cs
@@ -7,6 +7,7 @@ public class FireMinionMove : MonoBehaviour
     //Sets the value of speed in the Inspector
     [Header("Set in Inspector")]
     public float speed = 1f; //speed in m/s
+    public int points = 50;
 
     [HideInInspector]
     //variable for BoundsCheck
@@ -55,8 +56,15 @@ public class FireMinionMove : MonoBehaviour
             tempPos.y = (float)pos.y - 0.15f;
             fire.transform.position = tempPos;
         }
+<<<<<<< Updated upstream
         
         CheckBounds(); //checks if enemy is within the bounds of the screen
+=======
+        else if (counter == 200) {
+            //animatorController.SetBool("isAttack", false);
+        }
+        //CheckBounds(); //checks if enemy is within the bounds of the screen
+>>>>>>> Stashed changes
     }
 
     //Move method which moves the enemy down the screen at the given speed
