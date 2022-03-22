d76aba63ea9db0424543971c829051de3184a8f3
catrionachan <cpwychan@gmail.com>
2022-03-21 20:06:41 -0400

    more minor changes fireball

--- Restorations of Eldya/Assets/__Scripts/FireBallMover.cs
+++ Restorations of Eldya/Assets/__Scripts/FireBallMover.cs
@@ -8,8 +8,24 @@ public class FireBallMover : MonoBehaviour
     [Header("Set in Inspector")]
     public float speed = 2f; //speed in m/s
 
+<<<<<<< Updated upstream
     private int fireSpawn;
       
+=======
+    [HideInInspector]
+    //variable for BoundsCheck
+    public BoundsCheck bndCheck;
+
+    private int fireSpawn;
+    private int counter = 0;
+    //public Animator animatorController;
+    //Awake method that gets the Components of BoundsCheck
+    void Awake()
+    {
+        bndCheck = GetComponent<BoundsCheck>();
+    }
+
+>>>>>>> Stashed changes
     //Property of the Vector3 representing the position of the object
     public Vector3 pos
     {
@@ -48,7 +64,11 @@ public class FireBallMover : MonoBehaviour
     public virtual void CheckBounds()
     {
         ////if the enemy object is off the screen at the bottom, it destroys the object
+<<<<<<< Updated upstream
         
+=======
+        //if (bndCheck != null && pos.x=-3)
+>>>>>>> Stashed changes
 
         if (pos.x <= -5)
         {
