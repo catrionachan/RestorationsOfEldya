7085a62a5bbafe1c100798006641f0f2bd00a0d8
yilmazderin <dyilmaz2@uwo.ca>
2022-04-05 16:42:19 -0400

    bomb minion animations

--- Restorations of Eldya/Assets/__Scripts/BombMinionEnemy.cs
+++ Restorations of Eldya/Assets/__Scripts/BombMinionEnemy.cs
@@ -8,43 +8,27 @@ public class BombMinionEnemy : Enemy
     private bool ev = false;
     public float speed;
     public bool isFlipped = false;
+    public bool animate = false;
     public int counter = 0;
+    public int damage = 100;
 
     void Start()
     {
         healthBar.SetMaxHealth(maxHealth); //sets the inital health as max
-        
+        animate = false;
     }
 
     private void Update()
     {
         LookAtPlayer(); //Method to have the enemy follow the hero character
-
-        if (target.position.x >= transform.position.x-2)
-        {
-            anim.SetBool("OnEnter", true); //starts animator OnEnter
-            Debug.Log("startAnimation");
-            ev = true; //variable is set as try
-        }
-
+        PlayerEnters();
 
         if (ev == true) //move towards the hero character
         {
             transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
             counter++;
         }
-        if (counter == 800) {
-            anim.SetBool("Explode", true); //starts animator Explode
-            Explode();
-        }
 
-    }
-
-    //method to explode
-    public void Explode()
-    {
-        Debug.Log("fireExplosion");
-        //Instantiate(bullet, weaponMuzzle.position, weaponMuzzle.rotation);
     }
 
     public void LookAtPlayer()
@@ -71,5 +55,36 @@ public class BombMinionEnemy : Enemy
     {
         healthB.SetActive(false);//the healthBar object is removed from the screen
         Destroy(gameObject);//the character is removed from the screen 
+    }
+
+    public void PlayerEnters() {
+        if (target.position.x >= transform.position.x - 2)
+        {
+            anim.SetTrigger("OnEnter1"); //starts animator OnEnter
+            Debug.Log("startAnimation");
+            ev = true; //variable is set as try
+        }
+        if (counter == 500 && animate ==false) {
+            Debug.Log("fireExplosion");
+            anim.SetTrigger("OnEnter2"); //starts animator Explode
+            animate=true;
+        }
+        if (counter==520 ){
+            Die();
+        }
+
+    }
+    void OnTriggerEnter2D(Collider2D collider) 
+    {
+        if (collider.CompareTag("Player") && animate==true) 
+        {
+            PlayerMovement player = collider.GetComponent<PlayerMovement>();
+
+            if (player != null) //if the play is not null, player takes the damage
+            {
+                player.TakeDamage(damage);
+            }
+        }
+        
     }
 }
