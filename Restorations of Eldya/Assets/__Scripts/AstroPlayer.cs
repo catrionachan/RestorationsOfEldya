fc22a079df5b6a6c138f97b924661eef89861058
Vraj Shah <vshah86@uwo.ca>
2022-04-05 19:23:37 -0400

    Player Selection now works

---
+++ Restorations of Eldya/Assets/__Scripts/AstroPlayer.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AstroPlayer : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        //Builds and loads game on the start to the home page
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+
+    }
+}
